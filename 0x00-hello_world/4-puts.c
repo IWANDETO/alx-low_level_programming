fc363e34cabbb6e201aae98e017b73984f04178e
IWANDETO <wandettoian@gmail.com>
2022-09-08 16:57:01 -0700

    included a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
+/*
+ * main function- prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0;
+ */
 #include <stdio.h>
 
 int main(void)
 {
-        puts("Programming is like building a multilingual puzzle\n");
-        return (0);
+         puts("\"Programming is like building a multilingual puzzle");
+
+         return (0);
 }	
