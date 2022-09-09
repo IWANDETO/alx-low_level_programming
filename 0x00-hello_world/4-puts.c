cd8102307236edfbcd77f19c482312d132bd74ff
IWANDETO <wandettoian@gmail.com>
2022-09-08 17:04:33 -0700

    removed whites spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /*
- * main function- prints "Programming is like building a multilingual puzzle"
+ * main function-"Output Statement"
  *
  * Return: 0;
  */
-
-
 int main(void)
 {
          puts("\"Programming is like building a multilingual puzzle");
-
          return (0);
 }	
