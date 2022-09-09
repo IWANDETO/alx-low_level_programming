f7aa00870dd012d0f18ba846d5e80a383685ce90
IWANDETO <wandettoian@gmail.com>
2022-09-09 13:20:17 -0700

    aphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -program begins here
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
