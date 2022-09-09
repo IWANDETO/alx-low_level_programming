c6f08209ac22a6bb078e1b560d3bc1d8c7fc2325
IWANDETO <wandettoian@gmail.com>
2022-09-09 13:29:22 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -program begins here
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
