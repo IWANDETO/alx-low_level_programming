dd0ced6706f661a8ffc4600e1c4dbc575b6c7f01
IWANDETO <wandettoian@gmail.com>
2022-09-14 15:01:20 -0700

    Write a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - adds two integers and prints the result
+ * @a: integer to be added
+ * @b: integer to be added
+ * Return: prints sum; 0 otherwise
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
+
