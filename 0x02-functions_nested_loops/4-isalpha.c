26338cf5c1f37878155bdec50f14a3283595296d
IWANDETO <wandettoian@gmail.com>
2022-09-14 07:17:50 -0700

    Checked

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,9 +7,8 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
-	else if (c >= 65 && c <= 90)
-		return (1);
-	return (0);
+	else
+		return (0);
 }
