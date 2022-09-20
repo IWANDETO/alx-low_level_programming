25c115a2a21145d82d121a282dc6db0aa45626f0
IWANDETO <wandettoian@gmail.com>
2022-09-20 05:05:13 -0700

    Added the length interger

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,15 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int len;
 
-	while (*(str + i))
+	while (str[len] != '\0')
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
