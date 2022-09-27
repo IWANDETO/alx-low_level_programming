50959678f858ea467a3d2b8f0a3aa7f597e88b25
IWANDETO <wandettoian@gmail.com>
2022-09-27 06:30:17 -0700

    checked errors

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,11 +6,11 @@
  * @n: number of bytes
  * Return: the pointer to the dest
  */
-char *_memcpy(char dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
