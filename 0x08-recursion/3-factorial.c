37d5fa82e22a4160f1cf5508f44f18cbd3b86319
IWANDETO <wandettoian@gmail.com>
2022-09-28 15:00:09 -0700

    checked betty errors

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,13 +3,17 @@
  * factorial - function returns factorial of a number
  * @n: number whose factorial is checked
  * Return -1: Returns -1 when n is lower than 0
- * Reurn 0: Executed successfully
+ * Return: Returns 0 - Executed successfully
  */
 int factorial(int n)
 {
 	if (n < 0)
 	{
 		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
 	}
 	return (n * factorial(n - 1));
 }
