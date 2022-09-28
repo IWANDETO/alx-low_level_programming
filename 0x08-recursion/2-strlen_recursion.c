258d04a0a3e7dd597baad3b61e9d97f6f5f60e71
IWANDETO <wandettoian@gmail.com>
2022-09-28 14:38:17 -0700

    Function checks length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Function returns the length of a string
+ * @s: string whose length is checked and printed
+ * Return: Return 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
