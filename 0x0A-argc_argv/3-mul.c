13026dcbf99d3fbc757f2803a15fad89f2233049
IWANDETO <wandettoian@gmail.com>
2022-10-03 09:57:48 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function prints result of multiplication
+ * @argc: argument count
+ * @argv: string vector
+ * Return: 1 if there are no 2 arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	return (0);
+}
