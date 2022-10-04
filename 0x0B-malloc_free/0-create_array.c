404ef3b83386b1d78fe831dc26c7a5fd0dbff2dc
IWANDETO <wandettoian@gmail.com>
2022-10-04 14:28:11 -0700

    Checked Betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * *create_array - creates an array of chars and initializes it with a specific char
+ * create_array-creates an array of chars & initializes it with a specific char
  * @size: size of array
  * @c: specific char
  * Return: pointer to the array or NULL if it fails
