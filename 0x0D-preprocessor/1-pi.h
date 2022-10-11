7e0301fd397da358dd938b676ee6171908c7d337
IWANDETO <wandettoian@gmail.com>
2022-10-11 14:09:52 -0700

    task 1

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI
+#define PI
+
+/*
+ * File: 1-pi.h
+ * Desc: header file that defines a macro named
+ * PI as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
