5d59b9aebb4a67e9274d2f35a7a1046f73a0a480
IWANDETO <wandettoian@gmail.com>
2022-10-11 14:19:58 -0700

    Task 4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: header file that defines a macro named
+ * SUM(x, y) that computes the sum of x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
