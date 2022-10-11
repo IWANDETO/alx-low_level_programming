696ab0d608c93834dc2571e5cbaba03e91766791
IWANDETO <wandettoian@gmail.com>
2022-10-11 14:18:19 -0700

    task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Desc: header file that defines a macro named
+ * ABS(x) that computes the absolute value of x
+ */
+
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+
+#endif
