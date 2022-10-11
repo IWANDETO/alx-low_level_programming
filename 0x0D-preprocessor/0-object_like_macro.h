e602ee4a5d9d66096f7ca27a5a6d68de0bf9a3b4
IWANDETO <wandettoian@gmail.com>
2022-10-11 14:08:18 -0700

    Task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MACRO
+#define MACRO
+
+/*
+ * File: 0-object_like_macro.h
+ * Desc: header file that defines a macro named
+ * SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Readme file for Preprocessor in C
