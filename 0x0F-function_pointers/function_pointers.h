ffda414b4280692aebc0c5f77601cdecffc7a3f3
IWANDETO <wandettoian@gmail.com>
2022-10-13 11:31:21 -0700

    Included standard libraries

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef POINTERFUNCTIONS
 #define POINTERFUNCTIONS
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
