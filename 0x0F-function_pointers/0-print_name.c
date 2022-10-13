6347f00be6db4a2738243cc455506afe3853178a
IWANDETO <wandettoian@gmail.com>
2022-10-13 11:05:59 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* print_name - function that prints a name
+* @name: name of the person
+* @f: input pointer function
+* Return: return nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != 0 && name != 0)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef POINTERFUNCTIONS
+#define POINTERFUNCTIONS
+
+
+void print_name(char *name, void (*f)(char *));
+
+#endif 
