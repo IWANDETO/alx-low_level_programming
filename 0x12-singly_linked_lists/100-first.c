c64faa4a79fcc5a8fb941e11946525eaa56369a5
IWANDETO <wandettoian@gmail.com>
2022-10-25 15:23:38 -0700

    Advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * print_first - prints a sentence before the main
+ * Return: return nothing
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
