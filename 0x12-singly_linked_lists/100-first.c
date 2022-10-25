4e019c554a10c782b943dddfb13f772932866695
IWANDETO <wandettoian@gmail.com>
2022-10-25 15:25:09 -0700

    Rectified errors

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * print_first - prints a sentence before the main
  * Return: return nothing
  */
-void first(void)
+void print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
