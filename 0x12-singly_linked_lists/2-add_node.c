e0434aac090e9404b640e1f7ef18b54421e5be22
IWANDETO <wandettoian@gmail.com>
2022-10-25 14:55:08 -0700

    Errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to return
+ * Return: the length of the string
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
  * add_node - adds a new node at the beginning of list_t list
  * @head: address of node
  * @str: string to add
@@ -9,6 +24,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	int length = _strlen(str);
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
